fc48623e86ed0b0031f396fc230894235cfd9906
695518147 <695518147@qq.com>
2019-03-12 11:50:39 +0800

    member.php修改

--- ayangw/member.php
+++ ayangw/member.php
@@ -1,12 +1,11 @@
 <?php
-session_start();
 
 if (!defined('IN_CRONLITE')) exit();
 
 $my = isset($_GET['my']) ? $_GET['my'] : null;
 
 $clientip = real_ip();
-
+$islogin = 1;
 if (isset($_COOKIE["admin_token"])) {
     $islogin = 1;
 }
